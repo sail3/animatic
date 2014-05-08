17dc0b830384c68baaeb612ff4389d72ebae2ce6
sergio tenorio <kabyz_13@hotmail.com>
2014-05-08 15:10:59 -0500

    modificacion

--- source/animatic/portafolio.php
+++ source/animatic/portafolio.php
@@ -2,40 +2,141 @@
         <div class="wrapper">   <!-- ----------inicio de wrapper--------- -->     
             <?php include('sidebar.php'); ?>
                                  
-            <div class="main"> <!-- ----------inicio de wrapper--------- -->
+            <div class="main"> <!-- ----------inicio de main--------- -->
                 <div class="view view-first">
-                    <img src="img/1.jpg" />
+                    <img src="img/algarra.jpg" />
                     <div class="mask">
-                        <h2>Hover Style #1</h2>
-                        <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
+                        <h2>IMFORMACION</h2>
+                        <p>A wonderful serenity has taken possession of my entire soul.taken possession o  soul.</p>
                         <a href="#" class="info">Read More</a>
                     </div>
                 </div>  
                 <div class="view view-first">
-                    <img src="img/2.jpg" />
+                    <img src="img/florencia.jpg" />
                     <div class="mask">
-                        <h2>Hover Style #1</h2>
-                        <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
+                        <h2>IMFORMACION</h2>
+                        <p>A wonderful serenity has taken possession of my entire soul.taken possession of soul.</p>
                         <a href="#" class="info">Read More</a>
                     </div>
                 </div>  
                 <div class="view view-first">
-                    <img src="img/3.jpg" />
+                    <img src="img/balanze.jpg" />
                     <div class="mask">
-                        <h2>Hover Style #1</h2>
-                        <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
+                        <h2>IMFORMACION</h2>
+                        <p>A wonderful serenity has taken possession of my entire soul.taken possession o  soul.</p>
                         <a href="#" class="info">Read More</a>
                     </div>
                 </div>  
                 <div class="view view-first">
-                    <img src="img/4.jpg" />
+                    <a href="vinifan.php">
+                            <img src="img/vinifan2.jpg" />
+                        <div class="mask">
+                            <h2>IMFORMACION</h2>
+                            <p>A wonderful serenity has taken possession of my entire soul.taken possession of soul.</p>
+                            <a href="#" class="info">Read More</a>
+                        </div>
+                    </a>
+                </div>  
+                    <div class="view view-first">
+                    <img src="img/fiesta.jpg" />
                     <div class="mask">
-                        <h2>Hover Style #1</h2>
-                        <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
+                        <h2>IMFORMACION</h2>
+                        <p>A wonderful serenity has taken possession of my entire soul.taken possession o  soul.</p>
                         <a href="#" class="info">Read More</a>
                     </div>
                 </div>  
-            </div>   
+                <div class="view view-first">
+                    <img src="img/tottus.jpg" />
+                    <div class="mask">
+                        <h2>IMFORMACION</h2>
+                        <p>A wonderful serenity has taken possession of my entire soul.taken possession of soul.</p>
+                        <a href="#" class="info">Read More</a>
+                    </div>
+                </div>  
+                <div class="view view-first">
+                    <img src="img/borden.jpg" />
+                    <div class="mask">
+                        <h2>IMFORMACION</h2>
+                        <p>A wonderful serenity has taken possession of my entire soul.taken possession o  soul.</p>
+                        <a href="#" class="info">Read More</a>
+                    </div>
+                </div>  
+                <div class="view view-first">
+                    <img src="img/bells.jpg" />
+                    <div class="mask">
+                        <h2>IMFORMACION</h2>
+                        <p>A wonderful serenity has taken possession of my entire soul.taken possession of soul.</p>
+                        <a href="#" class="info">Read More</a>
+                    </div>
+                </div> 
+                    <div class="view view-first">
+                    <img src="img/rico.jpg" />
+                    <div class="mask">
+                        <h2>IMFORMACION</h2>
+                        <p>A wonderful serenity has taken possession of my entire soul.taken possession o  soul.</p>
+                        <a href="#" class="info">Read More</a>
+                    </div>
+                </div>  
+                <div class="view view-first">
+                    <img src="img/chips.jpg" />
+                    <div class="mask">
+                        <h2>IMFORMACION</h2>
+                        <p>A wonderful serenity has taken possession of my entire soul.taken possession of soul.</p>
+                        <a href="#" class="info">Read More</a>
+                    </div>
+                </div>  
+                <div class="view view-first">
+                    <img src="img/colanta.jpg" />
+                    <div class="mask">
+                        <h2>IMFORMACION</h2>
+                        <p>A wonderful serenity has taken possession of my entire soul.taken possession o  soul.</p>
+                        <a href="#" class="info">Read More</a>
+                    </div>
+                </div>  
+                <div class="view view-first">
+                    <img src="img/chicolac2.jpg" />
+                    <div class="mask">
+                        <h2>IMFORMACION</h2>
+                        <p>A wonderful serenity has taken possession of my entire soul.taken possession of soul.</p>
+                        <a href="#" class="info">Read More</a>
+                    </div>
+                </div> 
+                    <div class="view view-first">
+                    <img src="img/filler.jpg" />
+                    <div class="mask">
+                        <h2>IMFORMACION</h2>
+                        <p>A wonderful serenity has taken possession of my entire soul.taken possession o  soul.</p>
+                        <a href="#" class="info">Read More</a>
+                    </div>
+                </div>  
+                <div class="view view-first">
+                    <img src="img/alpesa.jpg" />
+                    <div class="mask">
+                        <h2>IMFORMACION</h2>
+                        <p>A wonderful serenity has taken possession of my entire soul.taken possession of soul.</p>
+                        <a href="#" class="info">Read More</a>
+                    </div>
+                </div>  
+                <div class="view view-first">
+                    <img src="img/polin.jpg" />
+                    <div class="mask">
+                        <h2>IMFORMACION</h2>
+                        <p>A wonderful serenity has taken possession of my entire soul.taken possession o  soul.</p>
+                        <a href="#" class="info">Read More</a>
+                    </div>
+                </div>  
+                <div class="view view-first">
+                    <img src="img/vamis.jpg" />
+                    <div class="mask">
+                        <h2>IMFORMACION</h2>
+                        <p>A wonderful serenity has taken possession of my entire soul.taken possession of soul.</p>
+                        <a href="#" class="info">Read More</a>
+                    </div>
+                </div> 
+            </div>   <!-- fin de main -->
+
+            <div class=" info"><p>INFORMACIÓN</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, ratione, nesciunt, nemo recusandae veniam magnam modi laboriosam illo consequatur in culpa dolorem corporis repellat eveniet dicta error illum. Asperiores, at.</p>
+            </div><!-- fin de info -->  
         </div>   
         <!-- ----------fin de wrapper--------- -->
 
