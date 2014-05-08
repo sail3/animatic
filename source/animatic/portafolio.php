845d282c195038728d7e7889b3f8e665d7e7f22a
Pedro Charun <mlosail3@gmail.com>
2014-05-08 13:14:09 -0500

    Modificada estructura de documentos, plantillas

---
+++ source/animatic/portafolio.php
@@ -0,0 +1,44 @@
+<?php include('header.php'); ?>
+        <div class="wrapper">   <!-- ----------inicio de wrapper--------- -->     
+            <?php include('sidebar.php'); ?>
+                                 
+            <div class="main"> <!-- ----------inicio de wrapper--------- -->
+                <div class="view view-first">
+                    <img src="img/1.jpg" />
+                    <div class="mask">
+                        <h2>Hover Style #1</h2>
+                        <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
+                        <a href="#" class="info">Read More</a>
+                    </div>
+                </div>  
+                <div class="view view-first">
+                    <img src="img/2.jpg" />
+                    <div class="mask">
+                        <h2>Hover Style #1</h2>
+                        <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
+                        <a href="#" class="info">Read More</a>
+                    </div>
+                </div>  
+                <div class="view view-first">
+                    <img src="img/3.jpg" />
+                    <div class="mask">
+                        <h2>Hover Style #1</h2>
+                        <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
+                        <a href="#" class="info">Read More</a>
+                    </div>
+                </div>  
+                <div class="view view-first">
+                    <img src="img/4.jpg" />
+                    <div class="mask">
+                        <h2>Hover Style #1</h2>
+                        <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
+                        <a href="#" class="info">Read More</a>
+                    </div>
+                </div>  
+            </div>   
+        </div>   
+        <!-- ----------fin de wrapper--------- -->
+
+<!-- Fin de las etiqeutas generales -->    
+</body>
+</html>
