ff2e1ea288dac2e861e33b4111a8226fe1936fc8
Pedro Charun <mlosail3@gmail.com>
2014-05-12 17:51:06 -0500

    Terminados los estilos de la pagina principal

--- source/animatic/index.php
+++ source/animatic/index.php
@@ -1,7 +1,7 @@
 <?php include('header.php'); ?>
  	<div class="wrapper">
 		<?php include('sidebar.php'); ?>
-	   	<section id="contenedor-main" class="main">
+	   	<section id="contenedor-main" >
 		   	<div class="flexslider">
 		        <ul class="slides">
 		            <li><img src="img/vinifan.jpg" /></li>    
@@ -15,33 +15,38 @@
 
 			<div class="productos">
 				<div class="section-productos">
-					<div class="sec1-item01"><a href="javascript:"><img src="" alt=""></a></div>
-					<div class="sec1-item02"><a href="javascript:"><img src="" alt=""></a></div>
-					<div class="sec1-item03"><a href="javascript:"><img src="" alt=""></a></div>
-					<div class="sec1-item04"><a href="javascript:"><img src="" alt=""></a></div>
-					<div class="sec1-item05"><a href="javascript:"><img src="" alt=""></a></div>
-					<div class="sec1-item06"><a href="javascript:"><img src="" alt=""></a></div>
+					
+					<div class="sec1-item01 bor-rad"><a href="javascript:"><img src="img/balanze-2.jpg" alt=""></a></div>
+					<div class="sec1-item02 bor-rad"><a href="javascript:"><img src="img/filler-2.jpg" alt=""></a></div>
+					<div class="sec1-item03 bor-rad"><a href="javascript:"><img src="img/suiza.jpg" alt=""></a></div>
+
+					<div class="sec1-item04 bor-rad"><a href="javascript:"><img src="img/layconsa.jpg" alt=""></a></div>
+					<div class="sec1-item05 bor-rad"><a href="javascript:"><img src="img/bells-2.jpg" alt=""></a></div>
+					<div class="sec1-item06 bor-rad"><a href="javascript:"><img src="img/fiesta2.jpg" alt=""></a></div>
 				</div>
-				<div class="section-productos">
-					<div class="sec2-item01"><a href="javascript:"><img src="" alt=""></a></div>
-					<div class="sec2-item02"><a href="javascript:"><img src="" alt=""></a></div>
-					<div class="sec2-item03"><a href="javascript:"><img src="" alt=""></a></div>
-					<div class="sec2-item04"><a href="javascript:"><img src="" alt=""></a></div>
-					<div class="sec2-item05"><a href="javascript:"><img src="" alt=""></a></div>
-					<div class="sec2-item06"><a href="javascript:"><img src="" alt=""></a></div>
+				<div class="clear"></div>
+				<div class="section-productos s2">
+					<div class="sec2-item01 bor-rad"><a href="javascript:"><img src="img/huerto-rico.jpg" alt=""></a></div>
+					<div class="sec2-item03 bor-rad"><a href="javascript:"><img src="img/papas.jpg" alt=""></a></div>
+					<div class="sec2-item02 bor-rad"><a href="javascript:"><img src="img/tacama.jpg" alt=""></a></div>
+					<div class="sec2-item04 bor-rad"><a href="javascript:"><img src="img/chicolac-2.jpg" alt=""></a></div>
+					<div class="sec2-item05 bor-rad"><a href="javascript:"><img src="img/vivanda.jpg" alt=""></a></div>
+					<div class="sec2-item06 bor-rad"><a href="javascript:"><img src="img/valle.jpg" alt=""></a></div>
 				</div>
+				<div class="clear"></div>
 				<div class="section-productos">
-					<div class="sec3-item01"><a href="javascript:"><img src="" alt=""></a></div>
-					<div class="sec3-item02"><a href="javascript:"><img src="" alt=""></a></div>
-					<div class="sec3-item03"><a href="javascript:"><img src="" alt=""></a></div>
-					<div class="sec3-item04"><a href="javascript:"><img src="" alt=""></a></div>
-					<div class="sec3-item05"><a href="javascript:"><img src="" alt=""></a></div>
-					<div class="sec3-item06"><a href="javascript:"><img src="" alt=""></a></div>
-					<div class="sec3-item07"><a href="javascript:"><img src="" alt=""></a></div>
+					<div class="sec3-item01 bor-rad"><a href="javascript:"><img src="img/yogur.jpg" alt=""></a></div>
+					<div class="sec3-item02 bor-rad"><a href="javascript:"><img src="img/bb.jpg" alt=""></a></div>
+					<div class="sec3-item03 bor-rad"><a href="javascript:"><img src="img/queso.jpg" alt=""></a></div>
+					<div class="sec3-item04 bor-rad"><a href="javascript:"><img src="img/fresa.jpg" alt=""></a></div>
+					<div class="sec3-item05 bor-rad"><a href="javascript:"><img src="img/rico-2.jpg" alt=""></a></div>
+					<div class="sec3-item06 bor-rad"><a href="javascript:"><img src="img/borden-2.jpg" alt=""></a></div>
+					<div class="sec3-item07 bor-rad"><a href="javascript:"><img src="img/aca.jpg" alt=""></a></div>
 				</div>
+				<div class="clear"></div>
 				<div class="section-productos">
-					<div class="sec4-item01"><a href="javascript:"><img src="" alt=""></a></div>
-					<div class="sec4-item02"><a href="javascript:"><img src="" alt=""></a></div>
+					<div class="sec4-item01 bor-rad"><a href="javascript:"><img src="img/vinifan-2.jpg" alt=""></a></div>
+					<div class="sec4-item02 bor-rad"><a href="javascript:"><img src="img/nugent.jpg" alt=""></a></div>
 				</div>
 			</div> <!-- FIN DE PRODUCTOS --> 
 		</section>   <!-- FIN DE SECTION -->
