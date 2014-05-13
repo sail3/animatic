219597d07f4414e90cba2126420706b8f6b8d9c5
Pedro Charun <mlosail3@gmail.com>
2014-05-13 16:27:19 -0500

    bugg fixing 16:27-13-05

--- source/animatic/vinifan.php
+++ source/animatic/vinifan.php
@@ -5,27 +5,29 @@
            
 
             <div class="vinifan-identidad">
-                <h1>Identidad de Marca</h1>      
-                <div class="main">                               
-                    <div class="identidad"><img src="img/info-vivifan.jpg" /></div>
-                </div>
-
-                 <div class=" info"><p>INFORMACIÓN</p><p>Estamos en condiciones de realizar esta tarea hábil creando diseños innovadores que se adapten a la perfección con sus valores centrales del negocio permitiendo que su empresa se ​​destaque aún más para un mayor potencial de éxito en un mercado altamente competitivo.</p>
-                </div><!-- fin de info -->  
-
-                 <div class="main">                               
-                    <div class="identidad"><img src="img/info-vivifan2.jpg" /></div>
-                </div>
-
-                 <div class=" info"><p>INFORMACIÓN</p><p>Estamos en condiciones de realizar esta tarea hábil creando diseños innovadores que se adapten a la perfección con sus valores centrales del negocio permitiendo que su empresa se ​​destaque aún más para un mayor potencial de éxito en un mercado altamente competitivo.</p>
-                </div><!-- fin de info -->  
-
-                 <div class="main">                               
-                    <div class="identidad"><img src="img/info-vivifan3.jpg" /></div>
-                </div>
-
-                 <div class=" info"><p>INFORMACIÓN</p><p>Estamos en condiciones de realizar esta tarea hábil creando diseños innovadores que se adapten a la perfección con sus valores centrales del negocio permitiendo que su empresa se ​​destaque aún más para un mayor potencial de éxito en un mercado altamente competitivo.</p>
-                </div><!-- fin de info -->  
+                <h1>Identidad de Marca</h1>
+                <article class="descripcion-item">
+                    <div class="main">                               
+                        <div class="identidad"><img src="img/info-vivifan.jpg" /></div>
+                    </div>
+                    <div class="info"><p>INFORMACIÓN</p><p>Estamos en condiciones de realizar esta tarea hábil creando diseños innovadores que se adapten a la perfección con sus valores centrales del negocio permitiendo que su empresa se ​​destaque aún más para un mayor potencial de éxito en un mercado altamente competitivo.</p>
+                    </div><!-- fin de info -->  
+                </article>    
+                
+                <article class="descripcion-item">
+                    <div class="main">                               
+                        <div class="identidad"><img src="img/info-vivifan2.jpg" /></div>
+                    </div>
+                    <div class="info"><p>INFORMACIÓN</p><p>Estamos en condiciones de realizar esta tarea hábil creando diseños innovadores que se adapten a la perfección con sus valores centrales del negocio permitiendo que su empresa se ​​destaque aún más para un mayor potencial de éxito en un mercado altamente competitivo.</p>
+                    </div><!-- fin de info -->  
+                </article>
+                <article class="descripcion-item">
+                    <div class="main">                               
+                        <div class="identidad"><img src="img/info-vivifan3.jpg" /></div>
+                    </div>
+                    <div class="info"><p>INFORMACIÓN</p><p>Estamos en condiciones de realizar esta tarea hábil creando diseños innovadores que se adapten a la perfección con sus valores centrales del negocio permitiendo que su empresa se ​​destaque aún más para un mayor potencial de éxito en un mercado altamente competitivo.</p>
+                    </div><!-- fin de info --> 
+                </article>
 
             </div>
 
